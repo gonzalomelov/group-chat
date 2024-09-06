774d2ecff1ccd0196e809d5ab402cd76712ca5cb
Gonzalo Melo <gonzalomelov@gmail.com>
2024-09-06 09:13:44 -0300

    chore: use normal version of OpenAiChatGpt but with prevous Message struct

--- src/abis/OpenAiChatGptABI.ts
+++
@@ -1,2 +0,0 @@
-import OpenAiChatGptABI from './OpenAiChatGpt.json' assert { type: "json" };
-export default OpenAiChatGptABI;
---
+++ src/abis/PrevOpenAiChatGptABI.ts
@@ -0,0 +1,2 @@
+import PrevOpenAiChatGptABI from './PrevOpenAiChatGpt.json' assert { type: "json" };
+export default PrevOpenAiChatGptABI;
